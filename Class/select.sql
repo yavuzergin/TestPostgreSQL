30b24bf00e72b103f4e716dc45d3941b9b12be0d
Yavuz <yvzerv@gmail.com>
2025-02-13 13:26:53 +0300

    Kişinin ait olduğu sınıfı sorgulayan sorgu düzeltildi.

--- Class/select.sql
+++ Class/select.sql
@@ -2,7 +2,7 @@ SELECT student_name, student_surname, class_name
 FROM students
 INNER JOIN classes
 ON students.class_id = classes.class_id
-WHERE student_name, student_surname = 'Rıza', 'Başıbüyük';
+WHERE student_name = 'Rıza' AND student_surname = 'Başıbüyük';
 
 SELECT student_name, student_surname
 FROM students
