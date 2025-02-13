d67268fe8f587df32d570093dc2283a2226359a2
Yavuz <yvzerv@gmail.com>
2025-02-13 16:11:17 +0300

    Gerekli düzeltmeler yapıldı.

--- market/create.sql
+++ market/create.sql
@@ -2,16 +2,16 @@ CREATE TABLE customers (
 customer_id SERIAL PRIMARY KEY,
 customer_name VARCHAR(100) NOT NULL,
 customer_surname VARCHAR(100) NOT NULL,
-customer_pnumber BIGINT NOT NULL,
+customer_phonenumber VARCHAR(50) NOT NULL,
 customer_email VARCHAR(255) NOT NULL,
-customer_adress TEXT NOT NULL
+customer_adress VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE products (
 product_id SERIAL PRIMARY KEY,
 product_name VARCHAR(255) NOT NULL,
-product_description TEXT NOT NULL,
-product_price INT NOT NULL,
+product_description VARCHAR(255) NOT NULL,
+product_price FLOAT NOT NULL,
 product_stock INT NOT NULL
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE orders (
 order_id SERIAL PRIMARY KEY,
 customer_id INT NOT NULL,
 product_id INT NOT NULL,
-total_price INT NOT NULL,
+total_price FLOAT NOT NULL,
 FOREIGN KEY (customer_id)
 REFERENCES customers (customer_id)
 FOREIGN KEY (product_id)
