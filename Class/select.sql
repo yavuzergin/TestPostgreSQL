a1706e4e65e8c54a9d1b39fd981d2c6c0da04e96
Yavuz <yvzerv@gmail.com>
2025-02-13 13:16:34 +0300

    Kişiye ait sınıf sorgusu, sınıfa ait öğrencilerin sorgusu ve sınıfa ait olmayan öğrencilerin sorgusu eklendi.

---
+++ Class/select.sql
@@ -0,0 +1,17 @@
+SELECT student_name, student_surname, class_name
+FROM students
+INNER JOIN classes
+ON students.class_id = classes.class_id
+WHERE student_name, student_surname = 'Rıza', 'Başıbüyük';
+
+SELECT student_name, student_surname
+FROM students
+INNER JOIN classes 
+ON students.class_id = classes.class_id
+WHERE classes.class_name = '1-A';
+
+SELECT student_name, student_surname
+FROM students
+INNER JOIN classes
+ON students.class_id = classes.class_id
+WHERE classes.class_name != '3-B';
