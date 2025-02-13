bdae9b3e403a329c205110c7c7282643910057ab
Yavuz <yvzerv@gmail.com>
2025-02-13 10:18:48 +0300

    book name, book_name olarak düzeltildi.

--- inserts.sql
+++ inserts.sql
@@ -1,20 +1,20 @@
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Dış Güçler' , 'Ekrem' , '2024-02-12');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('İç Güçler' , 'Ekrem' , '2024-02-13');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Güçler' , 'Ekrem' , '2024-02-14');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Güçsüzler' , 'Ekrem' , '2024-02-15');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Deneme - 1' , 'Mehmet' , '2024-02-16');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Öylesine' , 'Ali' , '2024-02-17');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Test Kitabı' , 'Ayşe' , '2024-02-18');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Kitap Testi' , 'Ayşe' , '2024-02-19');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('Kitap' , 'Rıza' , '2024-02-20');
-INSERT INTO books (book name, author, release_date)
+INSERT INTO books (book_name, author, release_date)
 VALUES ('İsimsiz Kitap' , 'Anonim' , '2024-02-21');
