f6499ec27fdf5d30e77179b4799b4b9679b89d3d
Yavuz <yvzerv@gmail.com>
2025-02-13 11:05:54 +0300

    Insert sorguları eklendi.

---
+++ Class/inserts.sql
@@ -0,0 +1,13 @@
+INSERT INTO classes (class_name)
+VALUES ('1-A');
+INSERT INTO classes (class_name)
+VALUES ('2-C');
+INSERT INTO classes (class_name)
+VALUES ('3-B');
+
+INSERT INTO students (student_name, student_surname, class_id)
+VALUES('Ali','Yılmaz','1');
+INSERT INTO students (student_name, student_surname, class_id)
+VALUES('Mehmet','Bölük','2');
+INSERT INTO students (student_name, student_surname, class_id)
+VALUES('Ahmet','Kulaksız','3');
